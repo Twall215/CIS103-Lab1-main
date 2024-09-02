5c7504870341da9fdeb0e1f5b769a727ee913a36
Trey Wallace <contacttreyw@gmail.com>
2024-09-01 21:08:34 -0500

    First full sentence

--- CIS103-Lab1-main/lab1.py
+++ CIS103-Lab1-main/lab1.py
@@ -3,7 +3,24 @@
 # Instructor: Md Ali
 # Student Name: Trey Wallace # Date: 9/1/2024
 # Description:
-# This script prints a greeting message to the user.
+# This script prints a personalized greeting message to the user.
+# and demonstrates the use of variables and basic data types.
 # --------------------------------------------------------
+#functions to ask the user their name and age
+def name():
+    x = input("Enter your name: ")
+    return x
 
-print("Hello, Welcome to CIS 103!")
+def age():
+    num = int(input("Enter your age:"))
+    return num
+
+
+def WusYaName():
+    return print(f"Your name is {name()} and you are {age()} years old")
+    
+
+
+
+if __name__ == "__main__":
+    WusYaName()
