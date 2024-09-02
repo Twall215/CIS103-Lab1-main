1aab2d77c9ca8fb48999ace48b05c75326ef9d16
Trey Wallace <contacttreyw@gmail.com>
2024-09-01 21:35:12 -0500

    Final product

--- CIS103-Lab1-main/lab1.py
+++ CIS103-Lab1-main/lab1.py
@@ -6,26 +6,25 @@
 # This script prints a personalized greeting message to the user.
 # and demonstrates the use of variables and basic data types.
 # --------------------------------------------------------
-#functions to ask the user their name and age
-def name():
-    x = input("Enter your name: ")
-    return x
 
-def age():
-    num = int(input("Enter your age:"))
-    return num
+# name and age as a variable instead of a function
+#feels easier to manage
 
-#Calculates when someone was born and how old they are 
+name = input("Enter your name: ")
+age = int(input("Enter your age:"))
+
+#Calculates when someone was born
 def whatyear():
     current_year = 2024
-    birth_year = current_year - age()
+    birth_year = current_year - age
     return birth_year
 
-def WusYaName():
-    return print(f"Your name is {name()}, you were born in {whatyear()} and you are {age()} years old")
+#The actual function
+def personalgreeting():
+    return print(f"Hello, {name}! You were born in {whatyear()} and you are {age} years old.")
     
 
 
 
 if __name__ == "__main__":
-    WusYaName()
+    personalgreeting()
